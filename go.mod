7cadbd0999be530bd7665c2c4fc2cef5ea016c73
youngbin yun <bintech.young@gmail.com>
2021-02-02 00:02:48 +0900

    jwt simulation

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module redis-template
 
 go 1.15
 
-require github.com/go-redis/redis/v8 v8.4.11
+require (
+	github.com/go-redis/redis/v8 v8.4.11
+	github.com/myesui/uuid v1.0.0 // indirect
+	github.com/twinj/uuid v1.0.0
+	gopkg.in/stretchr/testify.v1 v1.2.2 // indirect
+)
--- server.go
+++ server.go
@@ -3,11 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"strconv"
+	"time"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/twinj/uuid"
 )
 
 var ctx = context.Background()
+var rdb *redis.Client
 
 func ExampleClient() {
 	rdb := redis.NewClient(&redis.Options{
@@ -15,30 +19,41 @@ func ExampleClient() {
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
-
-	err := rdb.Set(ctx, "key", "value", 0).Err()
-	if err != nil {
-		panic(err)
-	}
-
-	val, err := rdb.Get(ctx, "key").Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("key", val)
-
-	val2, err := rdb.Get(ctx, "key2").Result()
-	if err == redis.Nil {
-		fmt.Println("key2 does not exist")
-	} else if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("key2", val2)
-	}
-	// Output: key value
-	// key2 does not exist
+	uuid := CreateAuth(rdb)
+	FetchAuth(uuid, rdb)
+	FetchAuth("strangecode", rdb)
 }
 
 func main() {
 	ExampleClient()
+}
+
+func CreateAuth(rds *redis.Client) string {
+	ExpiresAt := time.Now().Add(time.Minute * 10).Unix()
+	at := time.Unix(ExpiresAt, 0) //converting Unix to UTC
+	now := time.Now()
+	uuid := uuid.NewV4().String()
+	err := rds.Set(ctx, uuid, "1", at.Sub(now)).Err()
+	if err != nil {
+		fmt.Printf("%v", err)
+		panic(err)
+	}
+	return uuid
+}
+
+// 토큰에 저장된 uuid를 Redis에서 찾음
+func FetchAuth(uuid string, rds *redis.Client) {
+	id, err := rds.Get(ctx, uuid).Result()
+	// redis 에 저장된 적이 없거나 토큰의 유효 기간이 만료된 경우 에러 발생
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+		fmt.Println("키가 없는 값일 때 무엇을 리턴할까?")
+		fmt.Printf("non key id -> %v", id)
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("user id : ", id)
+	}
+	userID, _ := strconv.Atoi(id)
+	fmt.Println("user id : ", userID)
 }
